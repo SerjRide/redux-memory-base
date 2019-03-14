35ef76df25bc94fe7bdcadb704088411b33c6046
SerjRide <quicksquicks6@gmail.com>
2019-03-14 16:04:41 +0300

    added opportunity added bookmarks from category NEW

--- src/components/questions/player/player.js
+++ src/components/questions/player/player.js
@@ -157,23 +157,27 @@ class Player extends Component {
 
   addBookmarkBtn = () => {
     let categoryCount, questionCount, currentQuestion;
-    if (this.props.state[0] === 2222){
+    if (this.props.state[0] === 2222 || this.props.state[0] === 1111) {
+      let link
       const id = this.props.state[1]
-      const link = id + Math.pow(10,11)
+      if (this.props.state[0] === 1111) link = id + Math.pow(10,12)
+      if (this.props.state[0] === 2222) link = id + Math.pow(10,11)
       categoryCount = findCountById(link)
       questionCount = findCountById(link, false);
       const questionCountInNew = findCountById(this.props.state[1], false);
       currentQuestion = link;
       const length = QuestionData[1].length
 
-      if (length !== 2 || questionCountInNew !== length - 1) {
-        this.props.setQuestion(findId(1, questionCountInNew + 1))
-      }
+      if (this.props.state[0] === 2222) {
+        if (length !== 2 || questionCountInNew !== length - 1) {
+          this.props.setQuestion(findId(1, questionCountInNew + 1))
+        }
 
-      if (length === 2) {
-        this.props.update();
-      } else if (questionCountInNew === length - 1) {
-        this.props.setQuestion(findId(1, questionCountInNew - 1))
+        if (length === 2) {
+          this.props.update();
+        } else if (questionCountInNew === length - 1) {
+          this.props.setQuestion(findId(1, questionCountInNew - 1))
+        }
       }
 
     } else {
@@ -206,7 +210,9 @@ class Player extends Component {
     } else stock = `far`
 
     let disabled = ``;
+    let data_title = `Add to bookmarks`;
     if (categoryId === 1111 || categoryId === 2222) disabled = `disabled`;
+    if (categoryId === 2222) data_title = `Remove from bookmarks`
 
     return(
       <React.Fragment>
@@ -238,7 +244,7 @@ class Player extends Component {
            type="button"
            onClick={ this.addBookmarkBtn }
            className={`btn btn-secondary`}
-           data-title="Add to bookmarks">
+           data-title={ data_title }>
            <i className={`${stock} fa-bookmark`}></i>
         </button>
         <button
