5edcd24fa2f0769862545176f83a084483199792
SerjRide <quicksquicks6@gmail.com>
2019-03-16 12:17:57 +0300

    Updates

---
+++ precache-manifest.4bfdb09f3ceaf36d023bc549defd9068.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9ee1cf8c146156bf72",
+    "url": "/redux-memory-base/static/js/runtime~main.d5900391.js"
+  },
+  {
+    "revision": "01ae8e116cddbe0932af",
+    "url": "/redux-memory-base/static/js/main.dc869357.chunk.js"
+  },
+  {
+    "revision": "90086639101228ae9e11",
+    "url": "/redux-memory-base/static/js/2.95defdee.chunk.js"
+  },
+  {
+    "revision": "01ae8e116cddbe0932af",
+    "url": "/redux-memory-base/static/css/main.9eea2c1e.chunk.css"
+  },
+  {
+    "revision": "4a838d3e4bec48f4b953dc74038f03ce",
+    "url": "/redux-memory-base/index.html"
+  }
+];
