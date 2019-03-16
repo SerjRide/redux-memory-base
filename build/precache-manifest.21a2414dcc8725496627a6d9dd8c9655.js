b9645734eac06a19417d4a3f8ec02dbcc4bba858
SerjRide <quicksquicks6@gmail.com>
2019-03-16 17:51:21 +0300

    deploy on pages

---
+++ build/precache-manifest.21a2414dcc8725496627a6d9dd8c9655.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9ee1cf8c146156bf72",
+    "url": "/redux-memory-base/static/js/runtime~main.d5900391.js"
+  },
+  {
+    "revision": "058eecef096bbd324fdd",
+    "url": "/redux-memory-base/static/js/main.1c68e6cb.chunk.js"
+  },
+  {
+    "revision": "90086639101228ae9e11",
+    "url": "/redux-memory-base/static/js/2.95defdee.chunk.js"
+  },
+  {
+    "revision": "058eecef096bbd324fdd",
+    "url": "/redux-memory-base/static/css/main.f0dc6ca2.chunk.css"
+  },
+  {
+    "revision": "b2820c1f64a0c66955a3d0c211b89c1b",
+    "url": "/redux-memory-base/index.html"
+  }
+];
